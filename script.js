a21f42318836798716a1d821e437ae3305781001
shawna-donnelly <shawleigh17@gmail.com>
2024-11-10 23:46:25 -0700

    move styles to sass

--- script.js
+++ script.js
@@ -1,49 +1,94 @@
-// Skeleton Screen UI shown for 3s
+// Skeleton Screen UI shown for 3 seconds
 setTimeout(() => {
-  document.querySelectorAll(".hidden").forEach((el) => {
-    el.style.display = "block";
-  });
-
-  document.querySelectorAll(".skeleton").forEach((el) => {
-    el.style.display = "none";
-  });
+  document
+    .querySelectorAll(".skeleton")
+    .forEach((el) => (el.style.display = "none"));
+  document
+    .querySelectorAll(".hidden")
+    .forEach((el) => (el.style.display = "block"));
 }, 3000);
 
 // Function to check if page is scrolled and adjust the logo size
-
-const checkScroll = () => {
+function checkScroll() {
   const navbar = document.getElementById("navbar");
   const logo = document.getElementById("logo");
+  let scrollPosition = window.scrollY;
 
-  let scrollPos = window.scrollY;
-
-  // Add/remove scrolled class based on scroll position
-
-  if (scrollPos > 20) {
+  // Add/remove 'scrolled' class based on scroll position
+  if (scrollPosition > 20) {
     navbar.classList.add("scrolled");
-    logo.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
-    logo.classList.remove("scrolled");
   }
 
-  // Calculate the new font-size based on scroll position
-  let newSize = 3 - scrollPos * 0.03; // Descrease the font-size by 0.03 for every 1px scrolled
+  // Calculate new font size based on scroll position
+  let newSize = 3 - scrollPosition * 0.03; // Decrease by 0.03 rem for every 1px scrolled
 
-  // Clamping the font-size between 1.5 rem and 3rem;
+  // Clamping the font size between 1.5rem and 3rem
   newSize = Math.max(1.5, newSize);
+  newSize = Math.min(3, newSize);
 
   logo.style.fontSize = newSize + "rem";
-};
+}
 
-// Listen for scroll events
+// Event listener for scroll event
 window.addEventListener("scroll", checkScroll);
 
-setTimeout(() => {
-  const mainImage = document.getElementById("featured-article img");
+// Dark Mode
+const themeSwitcher = document.getElementById("theme-switcher");
 
-  mainImage.classList.remove("skeleton-featured-image");
-  //mainImage.classList.add("skeleton-featured-image");
+function updateThemeIcon(isDarkMode) {
+  themeSwitcher.children[0].classList.replace(
+    isDarkMode ? "fa-sun" : "fa-moon",
+    isDarkMode ? "fa-moon" : "fa-sun"
+  );
+}
 
-  mainImage;
-}, 3000);
+// Determine if dark mode is preferred
+function prefersDarkMode() {
+  return (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+}
+
+// Set the theme based on the preference
+function setThemeBasedOnPreference() {
+  const isDarkMode = prefersDarkMode();
+  document.documentElement.setAttribute(
+    "data-theme",
+    isDarkMode ? "dark" : "light"
+  );
+  updateThemeIcon(isDarkMode);
+}
+
+// Switch Theme manually
+function switchTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+  updateThemeIcon(newTheme === "dark");
+}
+
+// Event Listener
+themeSwitcher.addEventListener("click", switchTheme);
+
+// Initialize Theme
+function initializeTheme() {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    document.documentElement.setAttribute("data-theme", savedTheme);
+    updateThemeIcon(savedTheme === "dark");
+  } else {
+    setThemeBasedOnPreference();
+  }
+}
+
+// Listen for system theme changes
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", setThemeBasedOnPreference);
+
+// Initialize theme when the script loads
+initializeTheme();
